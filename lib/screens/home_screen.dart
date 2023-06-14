55f958164feb05369540364e2e5aa552f27aa0fc
EdwardRodriguezPeacedev <edward.rodriguez@peacedev.co>
2023-06-14 16:17:19 -0500

    add placeholder for not found image

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -31,16 +31,66 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: SafeArea(
           bottom: false,
-          child: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(),
-            child: Center(
-              child: catsModel == null
-                  ? const Center(child: CircularProgressIndicator())
-                  : Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children:
-                          List.generate(40, (index) => Text("data $index"))),
-            ),
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 15),
+                child: const Text(
+                  "Catbreed",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                ),
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  //physics: const NeverScrollableScrollPhysics(),
+                  //physics: const BouncingScrollPhysics(),
+                  child: Center(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: catsModel == null
+                            ? [const CircularProgressIndicator()]
+                            : List.generate(catsModel!.length, (index) {
+                                print(
+                                    "https://cdn2.thecatapi.com/images/${catsModel![index].referenceImageId}.jpg");
+                                return Card(
+                                  elevation: 10,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(12.0)),
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 20, vertical: 12),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(12.0),
+                                    ),
+                                    child: Column(
+                                      children: [
+                                        Image.network(
+                                          "https://cdn2.thecatapi.com/images/${catsModel![index].referenceImageId}.jpg",
+                                          width: double.infinity,
+                                          height: 200,
+                                          fit: BoxFit.fill,
+                                          errorBuilder:
+                                              (context, error, stackTrace) {
+                                            return Image.network(
+                                              "https://placekitten.com/200/300", // URL de imagen de reemplazo
+                                              width: double.infinity,
+                                              height: 200,
+                                              fit: BoxFit.cover,
+                                            );
+                                          },
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              })),
+                  ),
+                ),
+              ),
+            ],
           )),
     );
   }
