45c0c101ded27174105e3c4d8d3531f414191d5f
EdwardRodriguezPeacedev <edward.rodriguez@peacedev.co>
2023-06-14 15:08:01 -0500

    add cats model file

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,2 @@
-final hostApi = "https://api.thecatapi.com/v1/breeds";
-final xApiKey = "bda53789-d59e-46cd-9bc4-2936630fde39";
-
+const hostApi = "https://api.thecatapi.com/v1/breeds";
+const xApiKey = "bda53789-d59e-46cd-9bc4-2936630fde39";
